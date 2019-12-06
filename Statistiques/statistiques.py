02041ab0b4bb88db3d7ca9a47292482dbcf257fb
MAIRESSE GUILLAUME <gui.mairesse@gmail.com>
2019-12-06 11:01:40 +0100

    debut implementation stats

--- Statistiques/statistiques.py
+++ Statistiques/statistiques.py
@@ -2,6 +2,9 @@ from exo_entrées import data
 import argparse
 import sys
 import re
+import os
+import random
+# coding=<utf-8>
 
 
 if __name__=='__main__':
@@ -17,7 +20,7 @@ if __name__=='__main__':
     
     etats = data[exo]['etats']
 
-    print("Exercice choisis : %s"% exo)
+    print('Exercice choisis : %s'% exo)
     print('Nombre d\'états différents : %d'%data[exo]['nombre_etats'])
     
     for cle, etat in etats.items():
@@ -26,4 +29,39 @@ if __name__=='__main__':
         for trans, valeur in etat['transitions'].items():
             print('      ->', trans, ' : ', valeur)
 
+    objets_etat = int (input('Combien d\'objets par Etats?'))
+
+    for cle, etat in etats.items():
+        etat['nb_objets'] = objets_etat
+
+    iteration = int( input('Combien d\'itération?'))
+
+    history = [{}]
+    for cle, etat in etats.items():
+        temp = cle.split('_')
+        history[0][temp[1]] = etat['nb_objets']
+
+    for index in range(iteration):
+        futur = {}
+        for cle, etat in etats.items():
+            for objet in range(etat['nb_objets']):
+                randval = random.randint(0,100)
+                probacum = 0
+                print(randval)
+                for trans, valeur in etat['transitions'].items():
+                    if( probacum < randval <= probacum + valeur*100):
+                        if not(futur.get(trans)):
+                            futur[trans] = 0
+                        futur[trans] += 1
+                        print('oui')
+                    elif(99.99 < probacum <= 100):
+                        futur[trans] += 1
+                    else:
+                        print('non')
+                    probacum += valeur*100
+        history.append(futur)
+        for cle, etatfutur in futur.items():
+            etats['etat_'+cle]['nb_objets'] = etatfutur
+    for i in range(len(history)):
+        print('Iteration n°',i,' : ',history[i])
 
